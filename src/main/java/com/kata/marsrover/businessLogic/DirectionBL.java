213c097c214cafb032b0869f97598870e4b5d590
skander6 <khdhiriskander@gmail.com>
2023-01-21 12:45:13 +0100

    Preparing the test case for the getOppositeDirection

---
+++ src/main/java/com/kata/marsrover/businessLogic/DirectionBL.java
@@ -0,0 +1,10 @@
+package com.kata.marsrover.businessLogic;
+
+import com.kata.marsrover.model.Direction;
+
+public class DirectionBL {
+
+    public Direction getOppositeDirection(Direction direction) {
+        return Direction.NORTH;
+    }
+}
---
+++ src/test/java/com/kata/marsrover/businessLogic/DirectionBLTest.java
@@ -0,0 +1,12 @@
+package com.kata.marsrover.businessLogic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DirectionBLTest {
+
+    @Test
+    void getOppositeDirection() {
+    }
+}
