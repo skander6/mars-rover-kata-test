d859067058beb7ec95e9472e2f3cc21d5e68fc09
skander6 <khdhiriskander@gmail.com>
2023-01-21 11:39:32 +0100

    Rename Coordinates model+Refactoring+adding BL Signature

---
+++ src/main/java/com/kata/marsrover/businessLogic/GameBoardBL.java
@@ -0,0 +1,29 @@
+package com.kata.marsrover.businessLogic;
+
+import com.kata.marsrover.model.Direction;
+import com.kata.marsrover.model.GameBoard;
+
+public class GameBoardBL {
+
+    protected boolean move(Direction direction, GameBoard gameBoard) {
+       return false;
+    }
+
+
+    public boolean moveForward() {
+        return false;
+    }
+
+    public boolean moveBackward() {
+        return false;
+    }
+
+
+    public void changeDirectionLeft() {
+
+    }
+
+    public void changeDirectionRight() {
+
+    }
+}
--- src/main/java/com/kata/marsrover/model/Direction.java
+++ src/main/java/com/kata/marsrover/model/Direction.java
@@ -2,16 +2,20 @@ package com.kata.marsrover.model;
 
 public enum Direction {
 
-    NORTH('N'),
-    EAST('E'),
-    SOUTH('S'),
-    WEST('W');
-
+    NORTH(0, 'N'),
+    EAST(1, 'E'),
+    SOUTH(2, 'S'),
+    WEST(3, 'W');
+    private int value;
     private char tag;
 
-    private Direction(char tag) {
-
+    private Direction(int value, char tag) {
+        this.value = value;
         this.tag = tag;
+    }
+
+    public int getValue() {
+        return value;
     }
 
     public char getTag() { return tag; }
